69c1b10eecc7697354db72592eaecc3852fac310
pranit2400 <104119893+pranit2400@users.noreply.github.com>
2022-04-21 19:23:47 +0530

    Add files via upload

---
+++ Horizontal Line of Stars.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 21 19:20:22 2022
+
+@author: Pranit Dongare
+"""
+"""
+
+output:-
+
+* * * * * 
+
+"""
+
+for i in range(0,5):
+    print("*",end=" ")
