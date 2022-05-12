308afb874ae675e1d8652dad95e8af76f3b3aa37
pranit2400 <104119893+pranit2400@users.noreply.github.com>
2022-05-12 13:31:39 +0530

    Add files via upload

---
+++ Square.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 16:23:22 2022
+
+@author: Pranit Dongare
+"""
+"""
+output:-
+
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+"""
+row=column=5
+for i in range(0,row):
+    for j in range(0,column):
+        print("*",end=" ")
+    print("")
